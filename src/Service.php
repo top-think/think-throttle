3f4a6a1930c0490a92a709cd5812716a6ed2f304
hongweipeng <hongweichen8888@sina.com>
2019-10-15 10:28:23 +0800

    更新命名空间

--- src/Service.php
+++ src/Service.php
@@ -1,16 +1,9 @@
 <?php
-// +----------------------------------------------------------------------
-// | ThinkPHP [ WE CAN DO IT JUST THINK IT ]
-// +----------------------------------------------------------------------
-// | Copyright (c) 2006-2018 http://thinkphp.cn All rights reserved.
-// +----------------------------------------------------------------------
-// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
-// +----------------------------------------------------------------------
-// | Author: liu21st <liu21st@gmail.com>
-// +----------------------------------------------------------------------
-namespace think\middleware;
+
+namespace think\app;
 
 use think\Service as BaseService;
+use think\middleware\Throttle;
 
 class Service extends BaseService
 {
