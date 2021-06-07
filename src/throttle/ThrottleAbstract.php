6f43a815c7b06ddf1b5bf4707353c5359de7afd0
auooru <auooru@outlook.com>
2021-06-08 00:04:13 +0800

    声明缓存驱动变量类型

--- src/throttle/ThrottleAbstract.php
+++ src/throttle/ThrottleAbstract.php
@@ -4,6 +4,8 @@
 namespace think\middleware\throttle;
 
 
+use Psr\SimpleCache\CacheInterface;
+
 abstract class ThrottleAbstract
 {
     protected $cur_requests = 0;    // 当前已有的请求数
@@ -11,11 +13,11 @@ abstract class ThrottleAbstract
 
     /**
      * 是否允许访问
-     * @param string $key       缓存键
-     * @param float $micronow   当前时间戳,可含毫秒
-     * @param int $max_requests 允许最大请求数
-     * @param int $duration     限流时长
-     * @param $cache            缓存对象
+     * @param string $key           缓存键
+     * @param float $micronow       当前时间戳,可含毫秒
+     * @param int $max_requests     允许最大请求数
+     * @param int $duration         限流时长
+     * @param CacheInterface $cache 缓存对象
      * @return mixed
      */
     abstract public function allowRequest(string $key, float $micronow, int $max_requests, int $duration, $cache);
