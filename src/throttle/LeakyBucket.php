ebccb4cdcb714b90105c497fb15424813d3ec2d8
洪伟鹏 <hongweipeng@bonree.com>
2021-06-15 15:31:39 +0800

    修复漏桶算法中计算wait_seconds计算错误问题

--- src/throttle/LeakyBucket.php
+++ src/throttle/LeakyBucket.php
@@ -21,7 +21,7 @@ class LeakyBucket extends ThrottleAbstract
         $rate = (float) $duration / $max_requests;       // 平均 n 秒一个请求
         if ($micronow - $last_time < $rate) {
             $this->cur_requests = 1;
-            $this->wait_seconds = ceil($rate);
+            $this->wait_seconds = ceil($rate - ($micronow - $last_time));
             return false;
         }
 
