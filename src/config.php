a0d1ad226942dd67c1342c69cc50697227e4b47b
hongweipeng <hongweichen8888@sina.com>
2021-06-15 15:36:37 +0800

    配置文件添加设置算法驱动driver_name项

--- src/config.php
+++ src/config.php
@@ -3,6 +3,7 @@
 // | 节流设置
 // +----------------------------------------------------------------------
 use think\middleware\Throttle;
+use think\middleware\throttle\CounterFixed;
 use think\Request;
 use think\Response;
 
@@ -15,6 +16,14 @@ return [
     'visit_method' => ['GET', 'HEAD'],
     // 设置访问频率，例如 '10/m' 指的是允许每分钟请求10次;'10/60'指允许每60秒请求10次。值 null 表示不限制， eg: null 10/m  20/h  300/d 200/300
     'visit_rate' => '100/m',
+    /*
+     * 设置节流算法，组件提供了四种算法：
+     *  - CounterFixed ：计数固定窗口
+     *  - CounterSlider: 滑动窗口
+     *  - TokenBucket : 令牌桶算法
+     *  - LeakyBucket : 漏桶限流算法
+     */
+    'driver_name' => CounterFixed::class,
     // 响应体中设置速率限制的头部信息，含义见：https://docs.github.com/en/rest/overview/resources-in-the-rest-api#rate-limiting
     'visit_enable_show_rate_limit' => true,
     // 访问受限时返回的响应
