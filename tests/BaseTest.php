89f36138b9f0bb77438353a317dd0278acf34c33
hongweipeng <hongweichen8888@sina.com>
2023-06-13 14:52:54 +0800

    添加单元测试与 workflows 流程

---
+++ tests/BaseTest.php
@@ -0,0 +1,62 @@
+<?php
+namespace tests;
+use PHPUnit\Framework\TestCase;
+use think\App;
+
+class BaseTest extends TestCase {
+    static $ROOT_PATH = __DIR__ . "/../vendor/topthink/think";
+    static $RUNTIME_PATH = __DIR__ . "/../runtime/";
+    protected $app;
+    function __construct($name = null, array $data = [], $dataName = '')
+    {
+        parent::__construct($name, $data, $dataName);
+        $this->app = new App(static::$ROOT_PATH);
+        $this->app->setRuntimePath(static::$RUNTIME_PATH);
+
+    }
+
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+        // 每次测试完毕都需要清理 runtime cache 目录，避免影响其他单元测试
+        $cache_dir = $this->app->getRuntimePath() . "cache";
+        $dirs = glob($cache_dir . '/*', GLOB_ONLYDIR);
+        foreach ($dirs as $dir) {
+            $files = glob($dir . '/*.php');
+            foreach ($files as $file) {
+                unlink($file);
+            }
+        }
+        // 删除 cache 下的空目录
+        foreach ($dirs as $dir) {
+            rmdir($dir);
+        }
+
+    }
+
+    /**
+     * 加载中间件配置文件
+     * @param string $file 文件的路径 eg: $this->app->getBasePath() . 'middleware.php'
+     * @param string $type 类型：global 全局；route 路由；controller 控制器
+     */
+    function load_middleware(string $file, string $type = 'global') {
+        $this->app->middleware->import(include $file, $type);
+    }
+
+    /**
+     * 加载 throttle 配置文件
+     * @param string $file 文件路径
+     */
+    function load_throttle_config(string $file) {
+        $this->app->config->load($file, 'throttle');
+    }
+
+    /**
+     * 为避免警告 No tests found in class "tests\BaseTest".
+     * 添加一个恒为成功的测试
+     */
+    function test_always_success() {
+        $this->assertTrue(true);
+    }
+
+}
---
+++ tests/ThrottleDefaultConfigTest.php
@@ -0,0 +1,53 @@
+<?php
+
+
+namespace tests;
+
+
+/**
+ * 默认配置的单元测试
+ * Class ThrottleDefaultConfig
+ * @package tests
+ */
+class ThrottleDefaultConfigTest extends BaseTest
+{
+    public function __construct($name = null, array $data = [], $dataName = '')
+    {
+        parent::__construct($name, $data, $dataName);
+        $this->load_middleware(__DIR__ . "/config/global-middleware.php");
+        $this->load_throttle_config(dirname(__DIR__) . "/src/config.php");  // 加载默认配置
+    }
+
+    function test_visit_rate() {
+        // 默认的访问频率为 '100/m'
+        $allowCount = 0;
+        for ($i = 0; $i < 200; $i++) {
+            $request = new \think\Request();
+            $request->setMethod('GET');
+            $request->setUrl('/');
+
+            $response = $this->app->http->run($request);
+            if ($response->getCode() == 200) {
+                $allowCount++;
+            }
+        }
+        $this->assertEquals(100, $allowCount);
+    }
+
+    function test_unlimited_request_method() {
+        // 默认只限制了 ['GET', 'HEAD'] ，对 POST 不做限制
+        $allowCount = 0;
+        for ($i = 0; $i < 200; $i++) {
+            $request = new \think\Request();
+            $request->setMethod('POST');
+            $request->setUrl('/');
+
+            $response = $this->app->http->run($request);
+            if ($response->getCode() == 200) {
+                $allowCount++;
+            }
+        }
+        $this->assertEquals(200, $allowCount);
+    }
+
+}
